e672a117ac9ac58b58b619d0552c533e510468d8
Hurpeau Benoit <benoit.hurpeau@insee.fr>
2026-09-15 11:54:00 +0200

    Def A^tilde

--- IWS/Core/Relational.lean
+++ IWS/Core/Relational.lean
@@ -6,4 +6,16 @@ def adjacencyMatrix (G : Graph N) : Matrix (Fin N) (Fin N) ℝ := by
   classical
   exact fun i j => if G.Adj i j then 1 else 0
 
+def adjPlusIdentity (G : Graph N) : Matrix (Fin N) (Fin N) ℝ :=
+  adjacencyMatrix G + 1
+
+def rowSums (M : Matrix (Fin N) (Fin N) ℝ) (i : Fin N) : ℝ :=
+  ∑ j : Fin N, M i j
+
+def inverseDegMatrix (G : Graph (Fin N)) : Matrix (Fin N) (Fin N) ℝ :=
+  Matrix.diagonal fun i => (rowSums (adjPlusIdentity G) i)⁻¹
+
+def normalizedAdjacencyMatrix (G : Graph (Fin N)) : Matrix (Fin N) (Fin N) ℝ :=
+  inverseDegMatrix G * adjPlusIdentity G
+
 end IWS
